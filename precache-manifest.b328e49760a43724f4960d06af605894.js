fa1832896649160593e728adef7a6d345c0465ce
dost6ryanb <dost6.ryan.bantillo@gmail.com>
2020-04-01 12:33:03 +0800

    4/1/2020

---
+++ precache-manifest.b328e49760a43724f4960d06af605894.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b61e850ea50352a836",
+    "url": "/css/app.e1862071.css"
+  },
+  {
+    "revision": "a15708bacad837641782",
+    "url": "/css/chunk-vendors.db0cb4f0.css"
+  },
+  {
+    "revision": "3faeb1f32d7827b53fa0aeb74eff501f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5f059dfc57c10a647ba",
+    "url": "/js/about.50bad17d.js"
+  },
+  {
+    "revision": "25b61e850ea50352a836",
+    "url": "/js/app.36878864.js"
+  },
+  {
+    "revision": "a15708bacad837641782",
+    "url": "/js/chunk-vendors.c8a7daf9.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "78a0ec11d551f70919363378f378281d",
+    "url": "/sample_covid_cases.json"
+  },
+  {
+    "revision": "85df600d0fd30bb7256e981ce85a4b2a",
+    "url": "/sample_covid_cases_new.json"
+  },
+  {
+    "revision": "8663534233fe69d0a08150f04506cfcd",
+    "url": "/sample_covid_summary.json"
+  }
+]);
